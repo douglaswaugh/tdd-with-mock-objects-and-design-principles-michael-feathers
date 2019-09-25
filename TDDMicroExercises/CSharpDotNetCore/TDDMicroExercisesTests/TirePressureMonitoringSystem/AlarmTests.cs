eb093c3ad7978e19b4f5c329dfab59a3b1dc15ae
Douglas Waugh <douglas.waugh.e3@gmail.com>
2019-09-25 19:40:01 +0200

    Verifies Alarm is triggered when sensor reports pressure below threshold

--- TDDMicroExercises/CSharpDotNetCore/TDDMicroExercisesTests/TirePressureMonitoringSystem/AlarmTests.cs
+++ TDDMicroExercises/CSharpDotNetCore/TDDMicroExercisesTests/TirePressureMonitoringSystem/AlarmTests.cs
@@ -5,5 +5,16 @@ namespace TDDMicroExercisesTests.TirePressureMonitoringSystem
 {
     public class AlarmTests
     {
+        [Test]
+        public void Should_trigger_alarm_when_sensor_reports_pressure_below_threshold()
+        {
+            var sensor = new StubSensor(16);
+
+            var alarm = new Alarm(sensor);
+
+            alarm.Check();
+
+            Assert.That(alarm.AlarmOn, Is.True);
+        }
     }
 }
